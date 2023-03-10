f80997e52c3ba7c3f1b317f3d252439f15e8b5fa
Israel CHIZUNGU <chizunguisrael@gmail.com>
2023-03-10 10:27:36 +0200

    Fix syntax errors

--- schema.sql
+++ schema.sql
@@ -1,13 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
-create schema `vet_clinic`;
+create schema vet_clinic;
 
 CREATE TABLE animals (
-    id int not null auto_increment,
+    id SERIAL PRIMARY KEY,
     name varchar(100),
     date_of_birth date,
     escape_attempts int,
     neutered boolean,
-    weight_kg decimal,
-    primary key id
+    weight_kg decimal
 );
