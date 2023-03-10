12727ab2f5c7e222992f6dc772f294d68f5364f5
Israel CHIZUNGU <chizunguisrael@gmail.com>
2023-03-10 10:17:08 +0200

    Write database instructions

--- queries.sql
+++ queries.sql
@@ -1,3 +1,10 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * from animals WHERE name like '%mon';
+SELECT * from animals WHERE date_of_birth between '2016' and '2019';
+SELECT * from animals WHERE neutered = true and escape_attempts < 3;
+SELECT date_of_birth from animals WHERE name = 'Agumon' or name = 'Pikachu';
+SELECT name, escape_attempts from animals WHERE weight_kg > 10.5;
+SELECT * from animals WHERE neutered = true;
+SELECT * from annimals WHERE name !='Gabumon';
+SELECT * from animals WHERE weight_kg >= 10.4 and weight_kg <= 17.3;
--- schema.sql
+++ schema.sql
@@ -1,5 +1,13 @@
 /* Database schema to keep the structure of entire database. */
 
+create schema `vet_clinic`;
+
 CREATE TABLE animals (
-    name varchar(100)
+    id int not null auto_increment,
+    name varchar(100),
+    date_of_birth date,
+    escape_attempts int,
+    neutered boolean,
+    weight_kg decimal,
+    primary key id
 );
